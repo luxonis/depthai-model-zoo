fbe930ed89bb44404e54f09f2f21fca3163c09ec
Łukasz Piłatowski <lukpila29@gmail.com>
2021-12-01 14:27:40 +0100

    Add gifs and verbose names

--- website/populate_website.py
+++ website/populate_website.py
@@ -25,6 +25,8 @@ render_data = {
     "featured": [],
 }
 
+default_thumbnail = "https://user-images.githubusercontent.com/5244214/144071726-449a1264-5a63-44b6-91f8-6dba9da02576.png"
+
 for model_file in model_files:
     with model_file.open('r') as f:
         data = yaml.safe_load(f)
@@ -33,7 +35,7 @@ for model_file in model_files:
     data["name"] = model_file.parent.name
     data["resolution_factor"] = max([math.prod(map(int, input["shape"].split(',')[-2:])) for input in data["meta"]["inputs"]])
     if "thumbnail" not in data["meta"]:
-        data["meta"]["thumbnail"] = "https://user-images.githubusercontent.com/5244214/144071726-449a1264-5a63-44b6-91f8-6dba9da02576.png"
+        data["meta"]["thumbnail"] = default_thumbnail
     render_data["categories"][data["task_type"]] = render_data["categories"].get(data["task_type"], 0) + 1
     render_data["models"].append(data)
 
@@ -42,6 +44,7 @@ for model_file in model_files:
 
 
 render_data["categories"] = dict(sorted(render_data["categories"].items(), key=lambda item: item[1], reverse=True))
+render_data["models"] = sorted(render_data["models"], key=lambda item: (item["meta"].get("featured", False) * 10) + (item["meta"]["thumbnail"] != default_thumbnail), reverse=True)
 
 if output_path.exists():
     shutil.rmtree(output_path)
